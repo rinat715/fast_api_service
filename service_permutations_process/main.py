14d4ec4dd2b028bc3f57cb1c61a5ea190e9f22a9
Rinat Shaihetdinov <rinat.shaihetdinov@simbirsoft.com>
2023-05-29 10:46:06 +0500

    process pool

--- service_permutations_process/main.py
+++ service_permutations_process/main.py
@@ -1,37 +1,60 @@
+from asyncio import get_event_loop
+from concurrent.futures import ProcessPoolExecutor
+from contextlib import asynccontextmanager
 from typing import Any, Annotated
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Depends, Request, Query
 from fastapi.responses import JSONResponse
-
-import json
+from pydantic import BaseModel, validator, Json, ValidationError, parse_obj_as
 
 
 from service_permutations_process.permutations import permutations as service_permutations
 
-app = FastAPI()
 
-class DecodeErrorException(Exception):
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.executor = ProcessPoolExecutor()
+    yield
+    app.state.executor.shutdown()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+
+class ListRequeredErrorException(Exception):
     def __init__(self, name: str):
         self.name = name
 
 
 
-@app.exception_handler(DecodeErrorException)
-async def unicorn_exception_handler(request: Request, exc: DecodeErrorException):
+@app.exception_handler(ListRequeredErrorException)
+async def decode_exception_handler(_: Request, exc: ListRequeredErrorException):
     return JSONResponse(
-        status_code=418,
-        content={"message": f"Error! '{exc.name}' not valid json."},
+        status_code=418, # чайник
+        content={"message": f"Error! '{exc.name}' not list json"},
     )
 
 
-def parse_args(items: str) -> list[str, Any]:
+class Request(BaseModel):
+    items: list[Any]
+    
+    @validator('items', pre=True)
+    def items_only_list(cls, v):
+        if not isinstance(v, list):
+            raise ValueError('items only list')
+        return v
+
+
+def get_parsed_object(value: Json = Query(alias='items')):
     try:
-        return json.loads(items)
-    except json.JSONDecodeError:
-        raise DecodeErrorException(name=items)
+        return parse_obj_as(Request, {'items': value})
+    except ValidationError:
+        raise ListRequeredErrorException(name=value)
 
 
-def permutations(items: Annotated[list[str, Any], Depends(parse_args)]):
-   return service_permutations(items)
+def permutations(item: Annotated[Request, Depends(get_parsed_object)]) -> list[list[Any]]:
+   return service_permutations(item.items)
 
 
 @app.get("/health")
@@ -40,5 +63,14 @@ async def health() -> str:
 
 
 @app.get("/permutations_blocking")
-async def permutations_blocking(items: Annotated[list[str, Any], Depends(permutations)]) -> list[list[Any]]:
+async def permutations_blocking(items: Annotated[list[list[Any]], Depends(permutations)]):
+    return items
+
+
+async def run_in_process(item: Annotated[Request, Depends(get_parsed_object)]) -> list[list[Any]]:
+    return await get_event_loop().run_in_executor(app.state.executor, service_permutations, item.items)  
+
+
+@app.get("/permutations_non_blocking")
+async def permutations_non_blocking(items: Annotated[list[list[Any]], Depends(run_in_process)]):
     return items
